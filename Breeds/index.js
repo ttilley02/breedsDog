74a39741e7565092c4e872fda081305f6ae95179
ttilley02 <56703674+ttilley02@users.noreply.github.com>
2020-02-24 20:33:23 -0500

    Add files via upload

---
+++ Breeds/index.js
@@ -0,0 +1,49 @@
+'use strict';
+
+//Calls on the entire cast of functions to create the dog images specified by user input
+function getDogImage(dogBreed) {
+  fetch(' https://dog.ceo/api/breed/'+dogBreed+'/images/random')
+    .then(response => response.json())
+    .then(responseJson => 
+      displayResults(responseJson))
+    .catch(error => alert('That breed does not exist.  Please try again!'));
+  
+}
+
+
+function displayResults(responseJson) {
+  console.log(responseJson);
+  $('.placeholder').html('');
+  //creates dog collage for the nmber of dogs specified by the user
+  $('#errorMessage').replaceWith(`<img src=''>`)
+  if(responseJson.status == "success"){
+    $('.placeholder').append(`<img src='${responseJson.message}' class='results-img' alt='randomDogImage'>`)
+  }
+  else{
+    $('.placeholder').append(`<div id='errorMessage'>${responseJson.message}</div>`)
+    alert('No doggy data found!.  Please try again!');
+  }
+  
+
+  
+
+  //display the results section
+  $('.results').removeClass('hidden');
+}
+
+//grabs input (hopefully a intger 1-50) from user
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    console.log('WatchForm has ran!');
+    let dogType = $('.js-dogNumber').val();
+    getDogImage(dogType);
+  });
+}
+
+
+
+$(function() {
+  console.log('App loaded! Waiting for submit!');
+  watchForm();
+});
